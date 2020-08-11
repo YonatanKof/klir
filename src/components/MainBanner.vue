46c17984f5dbd01d16affd87fc1a98a00128fa75
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-08-11 18:24:56 +0300

    Init main banner

---
+++ src/components/MainBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-banner">
+    <img src="../assets/main-banner.jpg" alt="main banner" />
+  </div>
+</template>
+
+<style lang="scss">
+.main-banner {
+  overflow: hidden;
+  border-radius: 8px;
+  background-color: purple;
+  grid-area: main-banner;
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+}
+</style>
