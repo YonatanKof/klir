f972922bd0cfa5e5d895542757c0f40b21c478f9
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-08-11 20:19:27 +0300

    Add smal grid

---
+++ src/components/SmallBanner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="small-banner" style="--aspect-ratio: 1/1;">
+      Yo
+  </div>
+</template>
+
+<style lang="scss">
+.small-banner {
+  background-image: url(../assets/pic-01.png);
+  max-width: 100%;
+  max-height: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/SmallBannerGrid.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="small-banner-grid">
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+    <SmallBanner />
+  </div>
+</template>
+
+<script>
+import SmallBanner from "./SmallBanner.vue";
+
+export default {
+  name: "SmallBannerGrid",
+  components: {
+    SmallBanner,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+
+.small-banner-grid {
+  display: grid;
+  place-items: start;
+  grid-gap: 24px;
+  grid-template-columns: repeat(5, 1fr);
+  grid-area: small-banner-grid;
+  @media screen and (max-width: $width-lg) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 16px;
+  }
+  @media screen and (max-width: $width-md) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (max-width: $width-sm) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+.small-banner-grid > * {
+  width: 100%;
+}
+.small-banner-grid > [style^="--aspect-ratio"]::before {
+  content: "";
+  display: inline-block;
+  width: 1px;
+  height: 0;
+  padding-bottom: calc(100% / (var(--aspect-ratio)));
+}
+</style>
