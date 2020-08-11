a6906959dd84800c8009e8bb3ee0be91b3bf1267
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-08-11 18:25:21 +0300

    Int Main grid and sidebar

---
+++ src/components/MainGrid.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="main-grid">
+    <SideBar />
+    <MainBanner />
+    <SmallBannerGrid />
+  </section>
+</template>
+
+<script>
+import SideBar from "./SideBar.vue";
+import MainBanner from "./MainBanner.vue";
+import SmallBannerGrid from "./SmallBannerGrid.vue";
+
+export default {
+  name: "MainGrid",
+  components: {
+    SideBar,
+    MainBanner,
+    SmallBannerGrid,
+  },
+};
+</script>
+
+<style lang="scss">
+.main-grid {
+  display: grid;
+  background-color: powderblue;
+  padding: var(--main-padding);
+  max-width: var(--max-width);
+  margin: 0 auto;
+  grid-template-areas:
+    "sidebar main-banner"
+    "sidebar small-banner-grid";
+  grid-gap: 32px;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <aside class="side-bar">
+      Yo
+  </aside>
+</template>
+
+<style lang="scss">
+.side-bar {
+    width: 208px;
+    height: 416px;
+    background-color: var(--main-color);
+    grid-area: sidebar;
+}
+</style>
