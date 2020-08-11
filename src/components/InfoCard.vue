883ce9b248fea51bd59fa570530135c99430668e
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-08-11 21:23:43 +0300

    Add card

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <div class="card-info">
+      <h3>{{ cardTitle }}</h3>
+      <h2>{{ cardValue }}</h2>
+    </div>
+    <IconBase class :icon-name="cardIconName" icon-color="var(--main-strong)" width="48" height="48">
+      <slot />
+    </IconBase>
+  </div>
+</template>
+
+<script>
+import IconBase from "./IconBase.vue";
+// import IconKof from "./icons/IconKof.vue";
+
+export default {
+  name: "InfoCard",
+  props: ["cardTitle", "cardValue", "cardIconName"],
+  components: {
+    IconBase
+    // IconKof,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../styles/variables";
+
+.card {
+  padding: 16px;
+  width: 100%;
+  background-color: powderblue;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  .card-info {
+    *:first-child {
+      margin-bottom: 8px;
+    }
+  }
+  @media screen and (max-width: $width-sm) {
+    &:first-child {
+      margin-inline-end: 16px;
+    }
+  }
+}
+</style>
