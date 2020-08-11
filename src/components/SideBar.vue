f084c1183a89b94773d842d2a211b3785635a0fe
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-08-11 21:23:59 +0300

    Replace with card componenet

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -4,17 +4,30 @@
     <button>צרו הזמנה חדשה</button>
     <hr />
     <div class="cards">
-      <div class="card">
-        <h3>קו אשראי</h3>
-        <h2>₪20,000</h2>
-      </div>
-      <div class="card">
-        <h3>יתרת חשבון</h3>
-        <h2>₪2,048</h2>
-      </div>
+      <InfoCard card-title="קו אשראי" card-value="₪20,000" card-icon-name="Balance">
+        <IconDark />
+      </InfoCard>
+      <InfoCard card-title="יתרת חשבון" card-value="₪2,048" card-icon-name="Show me the money">
+        <IconBright />
+      </InfoCard>
     </div>
   </aside>
 </template>
+
+<script>
+import InfoCard from "./InfoCard.vue";
+import IconDark from "./icons/IconDark.vue";
+import IconBright from "./icons/IconBright.vue";
+
+export default {
+  name: "SideBar",
+  components: {
+    InfoCard,
+    IconDark,
+    IconBright
+  },
+};
+</script>
 
 <style lang="scss">
 @import "../styles/variables";
@@ -24,10 +37,10 @@
   height: max-content;
   grid-area: sidebar;
 }
-.card,
 button {
   padding: 16px;
   width: 100%;
+  font-size: 1em;
 }
 hr,
 button {
